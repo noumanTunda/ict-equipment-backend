340a2ee7fe745429b5049e8b2915a0ec9dd71852
noumanTunda <ntunda4000@gmail.com>
2026-08-24 11:46:19 +0300

    Feat: Added DotEnv Configuration to Load .env files before the app starts

--- src/main/java/com/tundalabs/ictequipment/IctEquipemntApplication.java
+++ src/main/java/com/tundalabs/ictequipment/IctEquipemntApplication.java
@@ -1,5 +1,6 @@
 package com.tundalabs.ictequipment;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IctEquipemntApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .filename(".env")
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
         SpringApplication.run(IctEquipemntApplication.class, args);
         System.out.println("Project is working");
     }
