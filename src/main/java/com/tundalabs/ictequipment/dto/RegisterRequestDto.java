ecbd232410a7d899877fc214268404439ea62b54
noumanTunda <ntunda4000@gmail.com>
2026-08-24 15:36:36 +0300

    Feat: Remved unused imports

--- src/main/java/com/tundalabs/ictequipment/dto/RegisterRequestDto.java
+++ src/main/java/com/tundalabs/ictequipment/dto/RegisterRequestDto.java
@@ -1,9 +1,7 @@
 package com.tundalabs.ictequipment.dto;
 
-import com.tundalabs.ictequipment.entity.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
