481ffb38cee3c046e1ee61219cdbbfc902f68085
noumanTunda <ntunda4000@gmail.com>
2026-09-09 12:53:24 +0300

    Feat: Added TransactionCode in Filtering Transaction

--- src/main/java/com/tundalabs/ictequipment/dto/TransactionFilterParams.java
+++ src/main/java/com/tundalabs/ictequipment/dto/TransactionFilterParams.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class TransactionFilterParams {
     private Long staffId;
+    private String transactionCode;
     private TransactionStatus status;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
