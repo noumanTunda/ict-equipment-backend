61da0c597a9a31118c4474cba3f2bf4e0b0c5a92
noumanTunda <ntunda4000@gmail.com>
2026-09-09 12:54:53 +0300

    Feat: Added findByStaffIdAndCompleted Query to filter Transactions

--- src/main/java/com/tundalabs/ictequipment/repository/TransactionIssuedItemRepository.java
+++ src/main/java/com/tundalabs/ictequipment/repository/TransactionIssuedItemRepository.java
@@ -2,6 +2,8 @@ package com.tundalabs.ictequipment.repository;
 
 import com.tundalabs.ictequipment.entity.TransactionIssuedItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface TransactionIssuedItemRepository extends JpaRepository<Transacti
     List<TransactionIssuedItem> findByTransactionId(Long transactionId);
 
     void deleteByTransactionId(Long transactionId);
+
+    @Query("SELECT tii FROM TransactionIssuedItem tii JOIN tii.transaction t WHERE t.staffId = :staffId AND t.status = 'COMPLETED'")
+    List<TransactionIssuedItem> findByStaffIdAndCompletedTransaction(@Param("staffId") Long staffId);
 }
