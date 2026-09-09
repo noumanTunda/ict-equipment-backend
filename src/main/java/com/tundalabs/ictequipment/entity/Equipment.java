4bef4a26de7d56016a18f74883a20486bc115485
noumanTunda <ntunda4000@gmail.com>
2026-09-09 12:48:08 +0300

    Feat: Removed assignedUserId from Equipments

--- src/main/java/com/tundalabs/ictequipment/entity/Equipment.java
+++ src/main/java/com/tundalabs/ictequipment/entity/Equipment.java
@@ -46,9 +46,6 @@ public class Equipment {
     @Column(name = "supplier_details", length = 255)
     private String supplierDetails;
 
-    @Column(name = "assigned_user_id")
-    private Long assignedUserId;
-
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
