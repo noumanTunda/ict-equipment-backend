566091ad2cf296b7c549140b004a0de46b64bd15
noumanTunda <ntunda4000@gmail.com>
2026-09-14 15:55:28 +0300

    Feat: Added Validation logic and checks to see if the Transaction has been Signed

--- src/main/java/com/tundalabs/ictequipment/service/impl/EquipmentTransactionServiceImpl.java
+++ src/main/java/com/tundalabs/ictequipment/service/impl/EquipmentTransactionServiceImpl.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
