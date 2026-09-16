434c1c1af4965a3a3b036d4549b2ca9928b08566
noumanTunda <ntunda4000@gmail.com>
2026-09-16 10:07:21 +0300

    Feat: Removed Signature Columns in Equipment Transactions Table

---
+++ src/main/resources/db/migration/V17__removed_unused_columns_in_equipment_transactions.sql
@@ -0,0 +1,12 @@
+alter table equipment_transactions
+drop column employee_signature;
+
+alter table equipment_transactions
+drop column employee_signed_at;
+
+alter table equipment_transactions
+drop column officer_signature;
+
+alter table equipment_transactions
+drop column officer_signed_at;
+
