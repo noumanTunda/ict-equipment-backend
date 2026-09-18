6f00fd78427eb97ed335c2a1a0a7492490b79455
noumanTunda <ntunda4000@gmail.com>
2026-09-18 16:32:08 +0300

    Feat: Added an Endpoint to check if current user has a Keyphrase set

--- src/main/java/com/tundalabs/ictequipment/controller/UserController.java
+++ src/main/java/com/tundalabs/ictequipment/controller/UserController.java
@@ -8,6 +8,7 @@ import com.tundalabs.ictequipment.entity.User;
 import com.tundalabs.ictequipment.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
