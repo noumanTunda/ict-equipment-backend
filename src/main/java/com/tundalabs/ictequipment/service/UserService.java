006e1a96a6fd1a65efd0abe12c1b07648c54838e
noumanTunda <ntunda4000@gmail.com>
2026-09-18 16:32:15 +0300

    Feat: Added an Endpoint to check if current user has a Keyphrase set

--- src/main/java/com/tundalabs/ictequipment/service/UserService.java
+++ src/main/java/com/tundalabs/ictequipment/service/UserService.java
@@ -13,4 +13,6 @@ public interface UserService {
     void setKeyphrase(Long userId, SetKeyphraseRequestDto request);
 
     void updateKeyphrase(Long userId, UpdateKeyphraseRequestDto request);
+
+    boolean hasKeyphrase(Long userId);
 }
